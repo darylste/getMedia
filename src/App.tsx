e66407a937205b4d9b539c529924abede05c6d83
Daryl Stensland <dstensland5@gmail.com>
2022-10-06 16:01:14 +0100

    banner component created

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import './styles/App.scss';
 
+import Banner from './atomic/molecules/banner/Banner.component';
 import Spacer from './atomic/atoms/spacer/Spacer.component';
-
 import Header from './atomic/molecules/header/Header.component';
 import Hero from './atomic/molecules/hero/Hero.component';
 
 function App() {
 	return (
 		<div className='App'>
-			<Spacer top='md' left='lg' right='lg'>
-				<Header />
-				<Hero />
-			</Spacer>
+			<Header />
+			<Hero />
+			<Banner />
 		</div>
 	);
 }
--- src/atomic/molecules/hero/Hero.component.tsx
+++ src/atomic/molecules/hero/Hero.component.tsx
@@ -8,30 +8,31 @@ import Spacer from '../../atoms/spacer/Spacer.component';
 const Hero: React.FC = () => {
 	return (
 		<>
-			<Spacer top='sm' />
-			<section className='hero'>
-				<div className='left'>
-					<Text varient='h1'>Find your favourites with GetMedia</Text>
+			<Spacer top='sm' bottom='md' left='md' right='md'>
+				<section className='hero'>
+					<div className='left'>
+						<Text varient='h1'>Find your favourites with GetMedia</Text>
 
-					<Spacer top='xs' bottom='xs'>
-						<Text varient='body'>
-							Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-							eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna
-							id volutpat lacus laoreet non curabitur gravida. Nullam eget felis
-							eget nunc lobortis. Sed risus ultricies tristique nulla aliquet
-							enim tortor at auctor. In pellentesque massa placerat duis
-							ultricies. Vitae tempus quam pellentesque nec nam aliquam sem. Eu
-							augue ut lectus arcu. Euismod lacinia at quis risus sed. Ipsum
-							nunc aliquet bibendum enim facilisis gravida neque. Fusce ut
-							placerat orci nulla pellentesque dignissim enim sit amet.
-						</Text>
-					</Spacer>
-					<Search />
-				</div>
-				<div className='right'>
-					<img className='hero-img' src='/music.svg' alt='' />
-				</div>
-			</section>
+						<Spacer top='xs' bottom='xs'>
+							<Text varient='body'>
+								Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+								eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna
+								id volutpat lacus laoreet non curabitur gravida. Nullam eget
+								felis eget nunc lobortis. Sed risus ultricies tristique nulla
+								aliquet enim tortor at auctor. In pellentesque massa placerat
+								duis ultricies. Vitae tempus quam pellentesque nec nam aliquam
+								sem. Eu augue ut lectus arcu. Euismod lacinia at quis risus sed.
+								Ipsum nunc aliquet bibendum enim facilisis gravida neque. Fusce
+								ut placerat orci nulla pellentesque dignissim enim sit amet.
+							</Text>
+						</Spacer>
+						<Search />
+					</div>
+					<div className='right'>
+						<img className='hero-img' src='/music.svg' alt='' />
+					</div>
+				</section>
+			</Spacer>
 		</>
 	);
 };
