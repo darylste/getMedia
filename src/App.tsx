053ebb24d8f8ae2af1e97232130109f8b9668f86
Daryl Stensland <dstensland5@gmail.com>
2022-10-06 14:12:00 +0100

    header component created

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,13 @@ import React from 'react';
 import './styles/App.scss';
 
 import Spacer from './atomic/atoms/spacer/Spacer.component';
-import Logo from './atomic/atoms/logo/Logo.component';
-import Nav from './atomic/atoms/nav/Nav.component';
+import Header from './atomic/molecules/header/Header.component';
 
 function App() {
 	return (
 		<div className='App'>
 			<Spacer top='md' left='lg' right='lg'>
-				<Logo />
-				<Nav></Nav>
+				<Header />
 			</Spacer>
 		</div>
 	);
