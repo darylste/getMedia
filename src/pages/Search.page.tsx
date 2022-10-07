17a3b136c0804c10d695260145ce39e23083aff1
Daryl Stensland <dstensland5@gmail.com>
2022-10-07 10:57:16 +0100

    added functionality to contact API for searhc featue

--- src/pages/Search.page.tsx
+++ src/pages/Search.page.tsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import axios from 'axios';
 
 const SearchPage: React.FC = () => {
-	return <div className='search'></div>;
+	interface ICollection {
+		artistName: string;
+		artworkUrl100: string;
+		collectionName: string;
+		previewUrl: string;
+		primaryGenreName: string;
+		releaseDate: string;
+	}
+	const [results, setResults] = useState<ICollection[]>([]);
+
+	const location = useLocation();
+	const term = location.search.split('=')[1];
+
+	useEffect(() => {
+		axios
+			.get(`https://itunes.apple.com/search?term=${term}&country=gb`)
+			.then((res) => setResults(res.data.results))
+			.catch((err) => console.log(err));
+	}, [term]);
+	console.log(results);
+	return (
+		<section>
+			{results.map((collection: ICollection) => (
+				<div className='card'>
+					<h4>{collection.artistName}</h4>
+					<h5>{collection.collectionName}</h5>
+					<p>
+						Year: {collection.releaseDate} Genre: {collection.primaryGenreName}
+					</p>
+				</div>
+			))}
+		</section>
+	);
 };
 
 export default SearchPage;
